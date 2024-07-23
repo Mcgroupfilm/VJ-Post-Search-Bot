7895165d29ab1911875714323e86ea1410ee2b8a
Mcgroupfilm <130358785+Mcgroupfilm@users.noreply.github.com>
2024-07-23 11:25:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,8 +9,8 @@ from os import environ
 
 API_ID       = int(environ.get("API_ID", "24482882"))
 API_HASH     = environ.get("API_HASH", "27bd653cd164958dbfe7c942546e018d")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "6607359975:AAFb1ejHutJWS07EEz49v2wLZIfS_JhlEAM")
 DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://thirankumar86:KpfFWdhOB0nh4I9F@cluster0.8q9rtk0.mongodb.net/?retryWrites=true&w=majority")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1001940969231"))
 ADMIN        = int(environ.get("ADMIN", "1180882237"))
 CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
